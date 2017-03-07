7a9a5e0c1ed15ca54aca46a7fb042dde08344627
JessicaNeary <Caneary@hotmail.com>
2017-03-08 12:47:30 +1300

    Added content for contact page

--- client/components/Contact/index.js
+++ client/components/Contact/index.js
@@ -3,7 +3,20 @@ import React from 'react'
 const Contact = () => {
   return(
     <div className='contact'>
-      <h1>Contact</h1>
+      <h1 className='title'>Contact</h1>
+      <div className='body'>
+        <p>
+          Pulse Networks Limited
+        </p>
+        <ul>
+          <li>PO Box 178, Orewa 0946</li>
+          <li>Phone - 64 09 974 4350</li>
+          <li>Fax - 64 09 426 0914</li>
+          <a href='mailto:info@pulse.co.nz'>
+            <li>info@pulse.co.nz</li>
+          </a>
+        </ul>
+      </div>
     </div>
   )
 }
