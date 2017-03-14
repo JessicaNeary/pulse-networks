70449a1fcf3a52577e5e02d85e399bc74b5612fa
JessicaNeary <Caneary@hotmail.com>
2017-03-14 13:47:25 +1300

    Adjusted styles for homepage and header, and changed fonts

--- client/components/Header.js
+++ client/components/Header.js
@@ -2,10 +2,9 @@ import React from 'react'
 import { Link } from 'react-router'
 
 const Header = () => {
-  return(
+  return (
     <div className='header'>
       <ul>
-        <li className='title'><h2>Pulse Networks</h2></li>
         <li><Link className='tab' to='/'>Home</Link></li>
         <li><Link className='tab' to='/services'>Services</Link></li>
         <li><Link className='tab' to='/projects'>Projects</Link></li>
--- client/components/Home/index.js
+++ client/components/Home/index.js
@@ -4,13 +4,13 @@ export default React.createClass({
   render () {
     return (
       <div className='home'>
-        <h3 className='title'>Welcome to Pulse Networks</h3>
+        <h3 className='title'>Pulse Networks</h3>
         <div className='body'>
           <p>
             High speed broadband connections are what drive economies, empowering business to reach their full potential and bringing communities closer together. Only Fibre Optics can provide true broadband with the speed, capacity and security demanded in the 21st century.
           </p>
           <p>
-            Pulse Networks provides a one stop shop for those wishing to build high speed networks that are scalable, future proofed and able to meet the exacting performance demanded by their clients. Thru it's association with Draka Comteq, the worlds largest manufacture of glass fibre, Pulse Networks brings to it's clients the very latest in technology and products. 
+            Pulse Networks provides a one stop shop for those wishing to build high speed networks that are scalable, future proofed and able to meet the exacting performance demanded by their clients. Thru it's association with Draka Comteq, the worlds largest manufacture of glass fibre, Pulse Networks brings to it's clients the very latest in technology and products.
           </p>
         </div>
       </div>
