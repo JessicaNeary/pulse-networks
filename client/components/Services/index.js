2c7cd1f7a8ecfed502bf2475bd1a39017585f7ee
JessicaNeary <Caneary@hotmail.com>
2017-03-15 16:10:31 +1300

    Added subheading icons to services page

--- client/components/Services/index.js
+++ client/components/Services/index.js
@@ -12,11 +12,26 @@ export default React.createClass({
         <Main />
 
         <div className='nav'>
-          <Link className='link 1' to='/services/1'>Planning & Design</Link>
-          <Link className='link 2' to='/services/3'>Installation</Link>
-          <Link className='link 3' to='/services/2'>Construction</Link>
-          <Link className='link 4' to='/services/4'>As Builds & Drafting</Link>
-          <Link className='link 5' to='/services/5'>Project Management</Link>
+          <div className='subheading'>
+            <Link className='link' to='/services/1'>Planning & Design</Link>
+            <img src='/images/services/presentation.svg' />
+          </div>
+          <div className='subheading'>
+            <Link className='link' to='/services/2'>Construction</Link>
+            <img src='/images/services/tools.svg' />
+          </div>
+          <div className='subheading'>
+            <Link className='link' to='/services/3'>Installation</Link>
+            <img src='/images/services/cone.svg' className='small' />
+          </div>
+          <div className='subheading'>
+            <Link className='link' to='/services/4'>As Builds & Drafting</Link>
+            <img src='/images/services/squares.svg' className='big' />
+          </div>
+          <div className='subheading'>
+            <Link className='link' to='/services/5'>Project Management</Link>
+            <img src='/images/services/folder.svg' className='big' />
+          </div>
         </div>
 
         <div className='content'>
