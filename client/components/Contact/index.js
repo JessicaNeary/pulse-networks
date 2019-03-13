7d6e0b6b8aac282a8802131aee486de50c6a4fd9
Jess Neary <jessicafneary@gmail.com>
2019-03-13 15:48:19 +1300

    Changed contract page to styled components and added transition

--- client/components/Contact/index.js
+++ client/components/Contact/index.js
@@ -1,24 +1,63 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+
+import { Heading, linkStyles } from "../SharedStyles.style";
+import posed from "react-pose";
+
+const Container = styled.div`
+  padding: 45px 0px 5px 0px;
+  font-family: "Roboto Condensed", sans-serif;
+  line-height: 1.3em;
+  color: #c4c2c2;
+`;
+
+const SubHeadingTransition = posed.p({
+  enter: { delay: 50, opacity: 1 },
+  exit: { opacity: 0 }
+});
+
+const SubHeading = styled(SubHeadingTransition)`
+  padding: 50px 0px 15px 0px;
+  font-size: 1.7em;
+`;
+
+const Info = posed.ul({
+  enter: { delay: 100, staggerChildren: 50 }
+});
+
+const LineTransition = posed.li({
+  enter: { y: 0, opacity: 1 },
+  exit: { y: 20, opacity: 0 }
+});
+
+const Line = styled(LineTransition)`
+  padding-top: 10px;
+  font-size: 1.1em;
+  margin-right: 40px;
+  list-style: none;
+`;
+
+const Link = styled.a`
+  ${linkStyles}
+`;
 
 const Contact = () => {
   return (
-    <div className='contact'>
-      <h1 className='title'>Contact</h1>
-      <div className='body'>
-        <p>
-          Pulse Networks Limited
-        </p>
-        <ul>
-          <li>PO Box 178, Orewa 0946</li>
-          <li>Unit C, 15 Douglas Alexander Parade, Albany</li>
-          <li>Phone - 64 09 974 4350</li>
-          <a className='link' href='mailto:info@pulse.co.nz'>
-            <li>info@pulse.co.nz</li>
-          </a>
-        </ul>
+    <Container>
+      <Heading>Contact</Heading>
+      <div>
+        <SubHeading>Pulse Networks Limited</SubHeading>
+        <Info>
+          <Line>PO Box 178, Orewa 0946</Line>
+          <Line>Unit C, 15 Douglas Alexander Parade, Albany</Line>
+          <Line>Phone - 64 09 974 4350</Line>
+          <Link href="mailto:info@pulse.co.nz">
+            <Line>info@pulse.co.nz</Line>
+          </Link>
+        </Info>
       </div>
-    </div>
-  )
-}
+    </Container>
+  );
+};
 
-export default Contact
+export default Contact;
