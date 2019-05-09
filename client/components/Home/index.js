43f89214198ad5f07821a0b00dc0037fec52c0f4
Jess Neary <jessicafneary@gmail.com>
2019-05-09 15:08:45 +1200

    Changed css to styled components

--- client/components/Home/index.js
+++ client/components/Home/index.js
@@ -1,32 +1,47 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+
+import { Heading, Copy } from "../SharedStyles.style";
+
+const Container = styled.div`
+  margin-left: 21%;
+  text-align: left;
+`;
+
+const Title = styled(Heading)`
+    padding-top: 30px;
+    width: 10%; }
+`;
+
+const Body = styled(Copy)`
+  padding: 0;
+  margin-left: -17px;
+  width: 80%;
+`;
 
 class Home extends React.Component {
-  render () {
+  render() {
     return (
-      <div className='home'>
-        <h3 className='title'>Pulse Networks</h3>
-        <div className='body'>
+      <Container>
+        <Title>Pulse Networks</Title>
+        <Body>
           <p>
-            {
-              `High speed broadband connections are what drive economies, empowering business to
+            {`High speed broadband connections are what drive economies, empowering business to
               reach their full potential and bringing communities closer together. Only Fibre
               Optics can provide true broadband with the speed, capacity and security demanded
-              in the 21st century.`
-            }
+              in the 21st century.`}
           </p>
           <p>
-            {
-            `Pulse Networks provides a one stop shop for those wishing to build high speed networks
+            {`Pulse Networks provides a one stop shop for those wishing to build high speed networks
              that are scalable, future proofed and able to meet the exacting performance demanded by
              their clients. Through our association with Draka Comteq, the worlds largest manufacture
              of glass fibre, we brings to our clients the very latest in technology and
-             products.`
-            }
+             products.`}
           </p>
-        </div>
-      </div>
-    )
+        </Body>
+      </Container>
+    );
   }
-};
+}
 
 export default Home;
