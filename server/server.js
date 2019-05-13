b696ae0b38d5c5c39eec21b1a2ef209af8294a35
Jess Neary <jessicafneary@gmail.com>
2019-05-13 16:25:41 +1200

    Adjusted package.json to call server and server to serve html on redirect

--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
-var path = require('path')
-var express = require('express')
-var bodyParser = require('body-parser')
+var path = require("path");
+var express = require("express");
+var bodyParser = require("body-parser");
 
-var server = express()
+var server = express();
 
-server.use(bodyParser.json())
-server.use(express.static(path.join(__dirname, '../public')))
+server.use(bodyParser.json());
+server.use(express.static(path.join(__dirname, "../public")));
 
-module.exports = server
+server.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../public/index.html"));
+});
+
+module.exports = server;
