d038e3b50c15887aaba95485e33d8f56a0d8f414
Jess Neary <jessicafneary@gmail.com>
2019-07-21 00:00:26 +1200

    removed unnecessary code

--- client/index.js
+++ client/index.js
@@ -1,27 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import App from "./components/App";
-import Projects from "./components/Projects";
-import Fibre from "./components/Projects/content/1-Fibre";
-import Telephony from "./components/Projects/content/2-Telephony";
-import CATV from "./components/Projects/content/3-CATV";
-import Radio from "./components/Projects/content/4-Radio";
-import Other from "./components/Projects/content/5-Other";
-import Contact from "./components/Contact";
 
 document.addEventListener("DOMContentLoaded", () => {
   ReactDOM.render(
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>,
     document.getElementById("app")
   );
 });
-
-// <Route path='1' component={Fibre} />
-// <Route path='2' component={Telephony} />
-// <Route path='3' component={CATV} />
-// <Route path='4' component={Radio} />
-// <Route path='5' component={Other} />
